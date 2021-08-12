b07dbc614e93db0f3bd051bc49723608f8c63d37
labi francis <moodyblerd@gmail.com>
2021-08-12 21:11:49 +0100

    contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,21 @@
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
+console.log(process.env.CONTENTFUL_ACCESS_TOKEN);
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `e4h5wcbnzqxz`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
+};
+
 // This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
 // import dotenv from 'dotenv';
 
